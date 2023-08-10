bcfbeced70b7bc01acd96ad3a8b3834577568be4
Kofi Owusu <k.o360@live.com>
2023-08-10 09:32:08 -0700

    first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main() 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
     printf("But the outcome is a piece of art,\n");
-    return 0;
+    return (0);
 }
+
