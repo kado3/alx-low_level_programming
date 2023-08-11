0b7ca3a01dbc605f33a7c5c338a645ffb47778eb
Kofi Owusu <k.o360@live.com>
2023-08-11 06:27:47 -0700

    first commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
-int main(void) {
-char lowercase = 'a';
-char uppercase = 'A';
-while (lowercase <= 'z') {
-putchar(lowercase);
-lowercase++;
-}
-while (uppercase <= 'Z') {
-putchar(uppercase);
-uppercase++;
-}
-putchar('\n');
-return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
