c29fb65b0d1800a2217e1f732a9840a975ae4362
Kofi Owusu <k.o360@live.com>
2023-08-11 06:25:13 -0700

    first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
-int main(void) {
-char letter = 'a';
-while (letter <= 'z') {
-putchar(letter);
-letter++;
-}
-putchar('\n');
-return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
