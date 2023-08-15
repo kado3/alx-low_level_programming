e725a9207c751d20bceb2538e709eff5d8d530eb
Kofi Owusu <k.o360@live.com>
2023-08-15 06:19:26 -0700

    first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,19 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet 10 times followed by new lines
- *
- * Return: void
+ * print_alphabet_x10 - Prints the lowercase alphabet ten times followed by new lines
  */
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-    }
+char letter;
+int i;
+for (i = 0; i < 10; i++)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
 }
