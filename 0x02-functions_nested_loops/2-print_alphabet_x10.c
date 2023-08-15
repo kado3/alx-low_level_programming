a750cdb032230885ad2f9a9fbef065cea7aa47af
Kofi Owusu <k.o360@live.com>
2023-08-15 06:32:16 -0700

    first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - Prints the lowercase alphabet ten times 
-* followed by new lines
+ * print_alphabet_x10 - Prints the lowercase alphabet ten times 
+ * followed by new lines
 */
 void print_alphabet_x10(void)
 {
