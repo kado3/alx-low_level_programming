5749eecb924d43323d27a897d58d89847445fc80
Kofi Owusu <k.o360@live.com>
2023-08-15 06:44:48 -0700

    first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- *print_alphabet_x10 - Prints the lowercase alphabet ten times 
- *followed by new lines
-*/
+ * print_alphabet_x10 - Prints the lowercase alphabet ten times 
+ * followed by new lines
+ */
 void print_alphabet_x10(void)
 {
-char letter;
-int i;
-for (i = 0; i < 10; i++)
-{
-letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
-}
+    char letter;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
+        _putchar('\n');
+    }
 }
