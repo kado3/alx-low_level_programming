ea64dec248976bd1f43ba55b70a3ae91f8c26c69
Kofi Owusu <k.o360@live.com>
2023-08-17 10:44:37 -0700

    first commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,13 @@
-_putchar('\n');
+#include "main.h"
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
 	}
 	else
 	{
