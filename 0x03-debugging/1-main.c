6d6442afd32087e592ecc99900f2d2471fae8667
Kofi Owusu <k.o360@live.com>
2023-08-17 01:58:41 -0700

    first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,13 +8,10 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-/** 
- * Commenting out the infinite loop
-* while (i < 10)
-* {
-* putchar(i);
-* }
-*/
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
