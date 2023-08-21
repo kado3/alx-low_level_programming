0c57cb8b7d5f73d598eb8cfe0bdb549ef8413a7c
Kofi Owusu <k.o360@live.com>
2023-08-21 09:17:26 -0700

    first commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * puts2 - Prints every other character of a string followed by a new line
+ * puts2 - Prints every other character of a string
  * @str: Pointer to a string
  */
 void puts2(char *str)
