c8e2815edfe97ba9c994272b502f861935631e23
Kofi Owusu <k.o360@live.com>
2023-08-30 03:50:57 -0700

    first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _puts_recursion - Prints a string, followed by a new line
- * @s: Pointer to a string
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: Pointer to the string to be printed
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-putchar('\n'); // Print a new line when the end of the string is reached
+    if (*s == '\0') // Base case: end of string
+    {
+putchar('\n');
 return;
 }
 putchar(*s); 
-_puts_recursion(s + 1); 
+_puts_recursion(s + 1);
 }
