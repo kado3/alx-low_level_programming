68d3a3904ff524907a751ea8f6342550cf7be5de
Kofi Owusu <k.o360@live.com>
2023-09-01 16:01:51 +0000

    first commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,6 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
+int result;
 if (argc != 3)
 {
 printf("Error\n");
@@ -17,7 +18,7 @@ return (1);
 }
 int num1 = atoi(argv[1]);
 int num2 = atoi(argv[2]);
-int result = num1 * num2;
+result = num1 * num2;
 printf("%d\n", result);
 return (0);
 }
