7bace003ccaa4af1aa0a00d3de3186ab6e0edff0
Kofi Owusu <k.o360@live.com>
2023-09-01 15:59:18 +0000

    first commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
  * main - Entry point
  * @argc: Number of command-line arguments
  * @argv: Array of command-line argument strings
  *
- * Return: Always 0
+ * Return: 0 for success, 1 for error
  */
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
+if (argc != 3)
 {
-printf("%s\n", argv[i]);
+printf("Error\n");
+return (1);
 }
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+int result = num1 * num2;
+printf("%d\n", result);
 return (0);
 }
