e6279045baf9daebead418991793e680912eecf4
Kofi Owusu <k.o360@live.com>
2023-09-01 16:10:12 +0000

    first commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,13 @@
 int main(int argc, char *argv[])
 {
 int sum = 0;
-for (int i = 1; i < argc; i++)
+int i;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
 {
 int num = atoi(argv[i]);
 if (num <= 0)
