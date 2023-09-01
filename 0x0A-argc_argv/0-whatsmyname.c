b40ed512be0b90915c4e5967a08c302a34c6dc46
Kofi Owusu <k.o360@live.com>
2023-09-01 15:15:07 +0000

    first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-int main(int argc, char *argv[]) {
-if (argc > 0) {
-printf("%s\n", argv[0]);
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+_puts(argv[0]);
 return (0);
 }
 return (1);
