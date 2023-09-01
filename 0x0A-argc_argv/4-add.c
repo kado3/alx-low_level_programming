f315087d03e38f038db75d7821f66fa7f1f52bc4
Kofi Owusu <k.o360@live.com>
2023-09-01 15:55:20 +0000

    first commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0 for success, 1 for error
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+for (int i = 1; i < argc; i++)
+{
+int num = atoi(argv[i]);
+if (num <= 0)
+{
+printf("Error\n");
+return (1);
+}
+sum += num;
+}
+printf("%d\n", sum);
+return (0);
+}
