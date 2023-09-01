96071e68a6afa2fdf5fdf08d5d1b162a38b29b05
Kofi Owusu <k.o360@live.com>
2023-09-01 15:18:36 +0000

    first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main - Entry point of the program
@@ -8,10 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-if (argc > 0)
-{
-_puts(argv[0]);
+(void)argc;
+printf("%s\n", argv[0]);
 return (0);
-}
-return (1);
 }
