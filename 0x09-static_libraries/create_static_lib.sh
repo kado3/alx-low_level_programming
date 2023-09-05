299ba51d94665085f795bb92fd69aa2a67094fd2
Kofi Owusu <k.o360@live.com>
2023-09-05 16:52:03 +0000

    first commit

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
+# Compile all .c files in the current directory
+gcc -c *.c
+# Create a static library from the compiled object files
 ar -rc liball.a *.o
+# Index the library for faster linking (optional but recommended)
 ranlib liball.a
+# Clean up the temporary object files
+rm *.o
+echo "Static library liball.a created successfully!"
