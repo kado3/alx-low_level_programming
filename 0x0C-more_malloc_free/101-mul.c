f9f7f305f30ca22dc99f55cdcc956b24f7ce9565
Kofi Owusu <k.o360@live.com>
2023-09-22 09:20:58 +0000

    first commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,45 +1,78 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <string.h>
 /**
-* is_digit - Check if a string is composed of digits
-* @str: The string to check
-*
-* Return: 1 if composed of digits, 0 otherwise
+* isNumber - checks if a string is a number
+* @s: string to check
+* Return: 1 if number, 0 if not
 */
-int is_digit(char *str)
+int isNumber(char *s)
 {
-while (*str)
-{
-if (*str < '0' || *str > '9')
+int i;
+for (i = 0; s[i]; i++)
+if (s[i] < '0' || s[i] > '9')
 return (0);
-str++;
-}
 return (1);
 }
 /**
-* main - Entry point
-* @argc: The number of command-line arguments
-* @argv: An array containing the command-line arguments
-*
-* Return: 0 on success, 98 on error
+* print_result - prints the result of the multiplication
+* @result: array of integers
+* @len: length of result
+* Return: void
 */
-int main(int argc, char *argv[])
+void print_result(int *result, int len)
 {
-if (argc != 3)
+int i;
+/* remove leading zeros */
+for (i = 0; i < len && result[i] == 0; i++);
+/* if all digits are 0, print 0 */
+if (i == len)
+printf("0");
+/* print the result */
+for (; i < len; i++)
+printf("%d", result[i]);
+printf("\n");
+}
+/**
+* main - multiplies two positive numbers
+* @ac: number of arguments
+* @av: array of arguments
+* Return: 0
+*/
+int main(int ac, char **av)
+{
+int i, j, num1_len, num2_len;
+int *result;
+if (ac != 3 || !isNumber(av[1]) || !isNumber(av[2]))
 {
 printf("Error\n");
-return (98);
+exit(98);
 }
-if (!is_digit(argv[1]) || !is_digit(argv[2]))
+num1_len = strlen(av[1]);
+num2_len = strlen(av[2]);
+result = calloc(num1_len + num2_len, sizeof(int));
+if (result == NULL)
 {
 printf("Error\n");
-return (98);
+exit(98);
 }
-/* Convert command-line arguments to integers and multiply */
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
-int result = num1 * num2;
-printf("%d\n", result);
+/* multiply each digit of num1 with num2 */
+for (i = num1_len - 1; i >= 0; i--)
+{
+for (j = num2_len - 1; j >= 0; j--)
+{
+/* multiply each digit and add to result */
+int mul = (av[1][i] - '0') * (av[2][j] - '0');
+/* add to previous result */
+int sum = result[i + j + 1] + mul;
+/* update result */
+result[i + j] += sum / 10;
+result[i + j + 1] = sum % 10;
+}
+}
+/* print result and free the allocated memory */
+print_result(result, num1_len + num2_len);
+free(result);
 return (0);
 }
