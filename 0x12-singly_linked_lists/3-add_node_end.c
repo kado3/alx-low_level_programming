2155272a12c342861275635cc52fd48354a034d2
Kofi Owusu <k.o360@live.com>
2023-09-22 16:28:03 +0000

    first commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,40 +1,33 @@
-#include "lists.h"
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 /**
-* add_node_end - Adds a new node at the end of a list_t list
-* @head: A pointer to a pointer to the head of the list
-* @str: The string to be duplicated and added to the new node
+* add_node_end - adds a new node at the end of a linked list
+* @head: double pointer to the list_t list
+* @str: string to put in the new node
 *
-* Return: The address of the new element, or NULL if it failed
+* Return: address of the new element, or NULL if it failed
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node;
-list_t *temp;
-if (str == NULL)
+list_t *new;
+list_t *temp = *head;
+unsigned int len = 0;
+while (str[len])
+len++;
+new = malloc(sizeof(list_t));
+if (!new)
 return (NULL);
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
-}
-new_node->len = strlen(str);
-new_node->next = NULL;
+new->str = strdup(str);
+new->len = len;
+new->next = NULL;
 if (*head == NULL)
 {
-*head = new_node;
+*head = new;
+return (new);
 }
-else
-{
-temp = *head;
-while (temp->next != NULL)
+while (temp->next)
 temp = temp->next;
-temp->next = new_node;
-}
-return (new_node);
+temp->next = new;
+return (new);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,17 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 /**
-* free_list - Frees a list_t list
-* @head: A pointer to the head of the list
+* free_list - frees a linked list
+* @head: list_t list to be freed
 */
 void free_list(list_t *head)
 {
-list_t *current;
-while (head != NULL)
+list_t *temp;
+while (head)
 {
-current = head;
-head = head->next;
-free(current->str);
-free(current);
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
 }
 }
