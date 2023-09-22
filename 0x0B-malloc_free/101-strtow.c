d69da01dc9e1da635fbacd317d2671e00f927391
Kofi Owusu <k.o360@live.com>
2023-09-22 10:24:48 +0000

    first commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,82 +1,50 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
-* wordcounter - counts words and the letters in them
-* @str: string to count
-* @pos: position of the word to count characters from
-* @firstchar: position of the first letter of the word
-* if pos = 0, count the number of chars in the word
-* else count number of words
-* Return: wordcount if pos == 0,
-* length of word if pos > 0,
-* position of word if pos > 0 && firstchar > 0
-*/
-int wordcounter(char *str, int pos, char firstchar)
-{
-int i, wordcount, charcount, flag;
-str[0] != ' ' ? (wordcount = 1) : (wordcount = 0);
-for (i = 0, flag = 0; str[i]; i++)
-{
-if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0' && flag == 0)
-{
-wordcount++;
-flag = 1;
-}
-if (pos > 0 && pos == wordcount)
-{
-if (pos > 0 && pos == wordcount && firstchar > 0)
-return (i);
-for (charcount = 0; str[i + charcount + 1] != ' '; charcount++);
-return (charcount);
-}
-if (str[i] == ' ')
-flag = 0;
-}
-return (wordcount);
-}
-/**
-* strtow - convert a string into a 2d array of words
-* @str: string to convert
-* Return: double pointer to 2d array
+* strtow - corta strings en palabras
+* @str: string a cortar en palabras
+* Return: puntero a palabras cortadas
 */
 char **strtow(char *str)
 {
-int wc, wordlen, getfirstchar, len, i, j;
 char **p;
-for (len = 0; str[len]; len++);
-if (str == NULL)
+int i, c, letras, spaces = 0;
+if (str == NULL || *str == '\0')
 return (NULL);
-wc = wordcounter(str, 0, 0);
-if (len == 0 || wc == 0)
-return (NULL);
-p = malloc((wc + 1) * sizeof(void *));
+i = 0;
+spaces = 0;
+while (str[i] != '\0')
+{
+if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+spaces++;
+i++;
+}
+p = malloc(sizeof(char *) * (spaces + 3));
 if (p == NULL)
 return (NULL);
-for (i = 0, wordlen = 0; i < wc; i++)
+c = 0;
+for (i = 0; str[i] != '\0' && c < spaces; i++)
 {
-/* Allocate memory for nested elements */
-wordlen = wordcounter(str, i + 1, 0);
-if (i == 0 && str[i] != ' ')
-wordlen++;
-p[i] = malloc(wordlen *sizeof(char) + 1);
-if (p[i] == NULL)
+if ((str[i] != ' ' && str[i] != '\0')||(str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0'))
 {
-for ( ; i >= 0; --i)
-free(p[i]);
-free(p);
-return (NULL);
+if (str[i] == ' ')
+i++;
+letras = 0;
+while (str[i] != '\0' && str[i] != ' ')
+{
+letras++;
+i++;
 }
-/* initialize each element of the nested array with the word*/
-getfirstchar = wordcounter(str, i + 1, 1);
-if (str[0] != ' ' && i > 0)
-getfirstchar++;
-else if (str[0] == ' ')
-getfirstchar++;
-for (j = 0; j < wordlen; j++)
-p[i][j] = str[getfirstchar + j];
-p[i][j] = '\0';
+p[c] = malloc(sizeof(char) * letras);
+i = i - letras;
+letras = 0;
+while (str[i] != '\0' && str[i] != ' ' && c != spaces)
+{
+p[c][letras] = str[i];
+i++;
+letras++;
 }
-p[i] = NULL;
+c++;
+}
+}
 return (p);
 }
