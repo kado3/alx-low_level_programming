7f5bf8d55d77e2c41ce158231556e910c8745fb7
Kofi Owusu <k.o360@live.com>
2023-09-28 15:21:18 +0000

    first commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
-* print_binary - function
-* @n: a
-* Return: Always 0.
+* print_binary - prints the binary equivalent of a decimal number
+* @n: number to print in binary
 */
 void print_binary(unsigned long int n)
 {
-unsigned long int copy = n;
-int len = 0;
-if (n == 0)
+int i, count = 0;
+unsigned long int current;
+for (i = 63; i >= 0; i--)
 {
+current = n >> i;
+if (current & 1)
+{
+_putchar('1');
+count++;
+}
+else if (count)
 _putchar('0');
-return;
 }
-while ((copy >>= 1) > 0)
-len++;
-while (len >= 0)
-{
-((n >> len) & 1) ? _putchar('1') : _putchar('0');
-len--;
-}
+if (!count)
+_putchar('0');
 }
