3f9d177f6a39f0e18422c25eeae3e6db9026992e
root <k.o360@live.com>
2023-12-15 11:45:42 -0800

    first commit

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: Pointer to a string
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+}
+putchar('\n');
+}
